997212b0aedf8fb500c738326af53d20bbb62f65
Jerome Habanz <jerome_jero@t-online.de>
2022-12-07 12:35:40 +0100

    SymbolTable & ProcedureBodyChecker

--- src/main/java/de/thm/mni/compilerbau/phases/_04a_tablebuild/TableBuilder.java
+++ src/main/java/de/thm/mni/compilerbau/phases/_04a_tablebuild/TableBuilder.java
@@ -1,15 +1,15 @@
 package de.thm.mni.compilerbau.phases._04a_tablebuild;
 
 import de.thm.mni.compilerbau.CommandLineOptions;
-import de.thm.mni.compilerbau.absyn.Expression;
-import de.thm.mni.compilerbau.absyn.Program;
-import de.thm.mni.compilerbau.absyn.TypeExpression;
-import de.thm.mni.compilerbau.absyn.Variable;
-import de.thm.mni.compilerbau.table.Identifier;
-import de.thm.mni.compilerbau.table.ProcedureEntry;
-import de.thm.mni.compilerbau.table.SymbolTable;
+import de.thm.mni.compilerbau.absyn.*;
+import de.thm.mni.compilerbau.absyn.visitor.DoNothingVisitor;
+import de.thm.mni.compilerbau.table.*;
+import de.thm.mni.compilerbau.types.ArrayType;
 import de.thm.mni.compilerbau.types.Type;
-import de.thm.mni.compilerbau.utils.NotImplemented;
+import de.thm.mni.compilerbau.utils.SplError;
+
+import java.util.List;
+import java.util.Optional;
 
 /**
  * This class is used to create and populate a {@link SymbolTable} containing entries for every symbol in the currently
@@ -19,17 +19,102 @@ import de.thm.mni.compilerbau.utils.NotImplemented;
  * Calculated {@link Type}s can be stored in and read from the dataType field of the {@link Expression},
  * {@link TypeExpression} or {@link Variable} classes.
  */
-public class TableBuilder {
+public class TableBuilder extends DoNothingVisitor {
     private final CommandLineOptions options;
+    private SymbolTable table;
 
     public TableBuilder(CommandLineOptions options) {
+        table = TableInitializer.initializeGlobalTable(options);
         this.options = options;
     }
 
     public SymbolTable buildSymbolTable(Program program) {
         //TODO (assignment 4a): Initialize a symbol table with all predefined symbols and fill it with user-defined symbols
+        program.accept(this);
 
-        throw new NotImplemented();
+        Optional<GlobalDeclaration> main = program.declarations.stream().filter(x -> x.name.equals(new Identifier("main"))).findAny();
+
+        if (main.isEmpty())
+            throw SplError.MainIsMissing();
+
+        if (!(main.get() instanceof ProcedureDeclaration))
+            throw SplError.MainIsNotAProcedure();
+
+        if (((ProcedureDeclaration) main.get()).parameters.size() != 0)
+            throw SplError.MainMustNotHaveParameters();
+
+        return this.table;
+    }
+
+    @Override
+    public void visit(Program program) {
+        program.declarations.forEach(x -> x.accept(this));
+    }
+
+    @Override
+    public void visit(ProcedureDeclaration procedureDeclaration) {
+        SymbolTable global = this.table;
+        table = new SymbolTable(this.table);
+
+        procedureDeclaration.parameters.forEach(para -> para.accept(this));
+        procedureDeclaration.variables.forEach(var -> var.accept(this));
+
+        List<ParameterType> parameterTypes = procedureDeclaration.parameters.stream().map(x -> new ParameterType(x.typeExpression.dataType, x.isReference)).toList();
+        ProcedureEntry entry = new ProcedureEntry(table, parameterTypes);
+
+        table = global;
+        table.enter(procedureDeclaration.name, entry, SplError.RedeclarationAsProcedure(procedureDeclaration.position, procedureDeclaration.name));
+
+        if (options.phaseOption == options.phaseOption.TABLES)
+            printSymbolTableAtEndOfProcedure(procedureDeclaration.name, entry);
+    }
+
+    @Override
+    public void visit(ParameterDeclaration parameterDeclaration) {
+        parameterDeclaration.typeExpression.accept(this);
+
+        if(parameterDeclaration.typeExpression.dataType instanceof ArrayType && !parameterDeclaration.isReference)
+            throw SplError.MustBeAReferenceParameter(parameterDeclaration.position, parameterDeclaration.name);
+
+        if(table.find(parameterDeclaration.name).isPresent())
+            throw SplError.RedeclarationAsParameter(parameterDeclaration.position, parameterDeclaration.name);
+
+        table.enter(parameterDeclaration.name,
+                new VariableEntry(parameterDeclaration.typeExpression.dataType, parameterDeclaration.isReference));
+    }
+
+    @Override
+    public void visit(VariableDeclaration variableDeclaration) {
+        variableDeclaration.typeExpression.accept(this);
+        table.enter(variableDeclaration.name,
+                new VariableEntry(variableDeclaration.typeExpression.dataType, false),
+                SplError.RedeclarationAsVariable(variableDeclaration.position, variableDeclaration.name));
+    }
+
+    public void visit(NamedTypeExpression namedTypeExpression) {
+        Optional<Entry> result = table.find(namedTypeExpression.name);
+        if(result.isEmpty())
+            throw SplError.UndefinedType(namedTypeExpression.position, namedTypeExpression.name);
+
+        Entry entry = result.get();
+        if(!(entry instanceof TypeEntry))
+            throw SplError.NotAType(namedTypeExpression.position, namedTypeExpression.name);
+        namedTypeExpression.dataType = ((TypeEntry) entry).type;
+    }
+
+    @Override
+    public void visit(ArrayTypeExpression arrayTypeExpression) {
+        arrayTypeExpression.baseType.accept(this);
+        arrayTypeExpression.dataType = new ArrayType(arrayTypeExpression.baseType.dataType, arrayTypeExpression.arraySize);
+    }
+
+    public void visit(TypeDeclaration typeDeclaration) {
+        if(table.find(typeDeclaration.name).isPresent())
+            throw SplError.RedeclarationAsType(typeDeclaration.position, typeDeclaration.name);
+        typeDeclaration.typeExpression.accept(this);
+        table.enter(typeDeclaration.name,
+                new TypeEntry(typeDeclaration.typeExpression.dataType),
+                SplError.RedeclarationAsType(typeDeclaration.position, typeDeclaration.name));
     }
 
     /**
--- src/main/java/de/thm/mni/compilerbau/phases/_04b_semant/ProcedureBodyChecker.java
+++ src/main/java/de/thm/mni/compilerbau/phases/_04b_semant/ProcedureBodyChecker.java
@@ -2,9 +2,17 @@ package de.thm.mni.compilerbau.phases._04b_semant;
 
 import de.thm.mni.compilerbau.CommandLineOptions;
 import de.thm.mni.compilerbau.absyn.*;
+import de.thm.mni.compilerbau.absyn.visitor.DoNothingVisitor;
+import de.thm.mni.compilerbau.table.Entry;
+import de.thm.mni.compilerbau.table.ProcedureEntry;
 import de.thm.mni.compilerbau.table.SymbolTable;
+import de.thm.mni.compilerbau.table.VariableEntry;
+import de.thm.mni.compilerbau.types.ArrayType;
+import de.thm.mni.compilerbau.types.PrimitiveType;
 import de.thm.mni.compilerbau.types.Type;
-import de.thm.mni.compilerbau.utils.NotImplemented;
+import de.thm.mni.compilerbau.utils.SplError;
+
+import java.util.Optional;
 
 /**
  * This class is used to check if the currently compiled SPL program is semantically valid.
@@ -12,18 +20,142 @@ import de.thm.mni.compilerbau.utils.NotImplemented;
  * Each node has to be checked for type issues or other semantic issues.
  * Calculated {@link Type}s can be stored in and read from the dataType field of the {@link Expression} and {@link Variable} classes.
  */
-public class ProcedureBodyChecker {
+public class ProcedureBodyChecker extends DoNothingVisitor {
 
     private final CommandLineOptions options;
+    SymbolTable table;
 
     public ProcedureBodyChecker(CommandLineOptions options) {
         this.options = options;
     }
 
     public void checkProcedures(Program program, SymbolTable globalTable) {
-        //TODO (assignment 4b): Check all procedure bodies for semantic errors
-
-        throw new NotImplemented();
+        this.table = globalTable;
+        program.declarations.stream().filter(x -> x instanceof ProcedureDeclaration).forEach(x -> x.accept(this));
     }
 
+    @Override
+    public void visit(ProcedureDeclaration procedureDeclaration) {
+        ProcedureEntry entry = (ProcedureEntry) table.lookup(procedureDeclaration.name);
+        SymbolTable global = table;
+        table = entry.localTable;
+        procedureDeclaration.body.forEach(x -> x.accept(this));
+        table = global;
+    }
+
+    @Override
+    public void visit(WhileStatement whileStatement) {
+        whileStatement.condition.accept(this);
+        if(whileStatement.condition.dataType != PrimitiveType.boolType)
+            throw SplError.WhileConditionMustBeBoolean(whileStatement.position, whileStatement.condition.dataType);
+        whileStatement.body.accept(this);
+    }
+
+    @Override
+    public void visit(IfStatement ifStatement) {
+        ifStatement.condition.accept(this);
+        if(ifStatement.condition.dataType != PrimitiveType.boolType)
+            throw SplError.IfConditionMustBeBoolean(ifStatement.position, ifStatement.condition.dataType);
+        ifStatement.thenPart.accept(this);
+        ifStatement.elsePart.accept(this);
+    }
+
+    @Override
+    public void visit(CallStatement callStatement) {
+        Optional<Entry> result = table.find(callStatement.procedureName);
+        if(result.isEmpty())
+            throw SplError.UndefinedProcedure(callStatement.position, callStatement.procedureName);
+        if(!(result.get() instanceof ProcedureEntry))
+            throw SplError.CallOfNonProcedure(callStatement.position, callStatement.procedureName);
+        ProcedureEntry entry = (ProcedureEntry) table.lookup(callStatement.procedureName);
+        callStatement.arguments.forEach(x -> x.accept(this));
+
+        if(callStatement.arguments.size() > entry.parameterTypes.size())
+            throw SplError.TooManyArguments(callStatement.position, callStatement.procedureName);
+        if(callStatement.arguments.size() < entry.parameterTypes.size())
+            throw SplError.TooFewArguments(callStatement.position, callStatement.procedureName);
+
+        for(int i = 0; i < callStatement.arguments.size(); i++) {
+            Expression arg = callStatement.arguments.get(i);
+            if(!arg.dataType.equals(entry.parameterTypes.get(i).type))
+                throw SplError.ArgumentTypeMismatch(arg.position,
+                        callStatement.procedureName,
+                        i + 1,
+                        entry.parameterTypes.get(i).type,
+                        arg.dataType);
+            if(entry.parameterTypes.get(i).isReference && !(arg instanceof VariableExpression))
+                throw SplError.ArgumentMustBeAVariable(arg.position, callStatement.procedureName, i + 1);
+        }
+    }
+
+    @Override
+    public void visit(UnaryExpression unaryExpression) {
+        unaryExpression.operand.accept(this);
+        if(unaryExpression.operand.dataType == PrimitiveType.boolType)
+            throw SplError.NoSuchOperator(unaryExpression.position, unaryExpression.operator, unaryExpression.dataType);
+        unaryExpression.dataType = unaryExpression.operand.dataType;
+    }
+
+    @Override
+    public void visit(BinaryExpression binaryExpression) {
+        binaryExpression.leftOperand.accept(this);
+        binaryExpression.rightOperand.accept(this);
+        binaryExpression.dataType = binaryExpression.operator.isArithmetic() ? PrimitiveType.intType : PrimitiveType.boolType;
+        if(!(binaryExpression.leftOperand.dataType.equals(binaryExpression.rightOperand.dataType))) {
+            throw SplError.NoSuchOperator(binaryExpression.position,
+                    binaryExpression.operator,
+                    binaryExpression.leftOperand.dataType,
+                    binaryExpression.rightOperand.dataType);
+        }
+    }
+
+    @Override
+    public void visit(CompoundStatement compoundStatement) {
+        compoundStatement.statements.forEach(x -> x.accept(this));
+    }
+
+    @Override
+    public void visit(IntLiteral intLiteral) {
+        intLiteral.dataType = PrimitiveType.intType;
+    }
+
+    @Override
+    public void visit(AssignStatement assignStatement) {
+        assignStatement.target.accept(this);
+        assignStatement.value.accept(this);
+
+        if(assignStatement.target.dataType instanceof ArrayType)
+            throw SplError.IllegalAssignmentToArray(assignStatement.position);
+
+        if(!assignStatement.target.dataType.equals(assignStatement.value.dataType))
+            throw SplError.IllegalAssignment(assignStatement.position,
+                    assignStatement.target.dataType,
+                    assignStatement.target.dataType);
+    }
+
+    @Override
+    public void visit(VariableExpression variableExpression) {
+        variableExpression.variable.accept(this);
+        variableExpression.dataType = variableExpression.variable.dataType;
+    }
+
+    @Override
+    public void visit(NamedVariable namedVariable) {
+        Entry entry = table.lookup(namedVariable.name);
+        if(!(entry instanceof VariableEntry))
+            throw SplError.NotAVariable(namedVariable.position, namedVariable.name);
+        namedVariable.dataType = ((VariableEntry) entry).type;
+    }
+
+    @Override
+    public void visit(ArrayAccess arrayAccess) {
+        arrayAccess.index.accept(this);
+        if(!arrayAccess.index.dataType.equals(PrimitiveType.intType))
+            throw SplError.IndexingWithNonInteger(arrayAccess.position);
+        arrayAccess.array.accept(this);
+        if(arrayAccess.array.dataType instanceof ArrayType)
+            arrayAccess.dataType = ((ArrayType) arrayAccess.array.dataType).baseType;
+        else
+            throw SplError.IndexingNonArray(arrayAccess.position);
+    }
 }
